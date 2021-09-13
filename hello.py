06edd3e3ec26f91421ae3d09d80023dd7457a4e0
EC2 Default User <ec2-user@ip-172-31-13-181.ec2.internal>
2021-09-13 09:13:21 +0000

    As per test project requirement

--- hello.py
+++ hello.py
@@ -2,5 +2,15 @@ def add(x, y):
     return x + y
 
 
+
+def toyou(x):
+    return f"hi {x}"
+
+def add1(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
+
 print(1, 2)
 print(f"the sum of 1 and 2 is {add(1,2)}")
